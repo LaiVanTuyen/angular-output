08103bf9a14d64856dfc6ae0c98a6d199ff5bf76
Lại Văn Tuyên <vantuyen24032001@gmail.com>
2023-05-07 17:34:39 +0700

    angular-output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-output';
+
+  // countParent;
+  // countChangeHandler(count: number){
+  //   this.countParent = count;
+  // }
+
 }
